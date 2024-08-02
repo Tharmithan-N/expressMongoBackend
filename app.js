ee5472aea388b50319408082e122ce69395305df
Tharmithan <tharmithan@orelit.com>
2024-08-02 09:05:09 +0530

    Backend working crud added

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const express = require("express");
 
-express().listen(5000, () => {
-  console.log("Server started on port 3000");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const userRoutes = require("./routes/user");
+const subjectRoutes = require("./routes/subject");
+const env = require("dotenv/config");
+
+app.use("/api/", userRoutes);
+app.use("/learn/", subjectRoutes);
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
